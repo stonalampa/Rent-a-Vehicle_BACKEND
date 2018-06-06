c44f8929121ea9ce708197c7b416d7055286dcad
Milan Radanovic <milan.rockstar88@gmail.com>
2018-06-06 13:08:33 +0200

    napravljen controller

---
+++ RentApp/Controllers/GetUserController.cs
@@ -0,0 +1,27 @@
+﻿using RentApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentApp.Controllers
+{
+    public class GetUserController : Controller
+    {
+        [HttpGet]
+        public ActionResult ReturnUser()
+        {
+            AppUser testUser = new AppUser();
+            testUser.Id = 1;
+            testUser.PhotoPath = "nema za sad";
+            testUser.Type = UserType.ADMIN;
+            testUser.FullName = "Stojan Stojanovic";
+            testUser.dateOfBirth = DateTime.Now;
+
+            return View();
+        }
+
+        
+    }
+}
